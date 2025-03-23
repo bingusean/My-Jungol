6bd029f77a950322b58ed95aa6fcd2a531e1e310
sihyeonkim <bingusean@outlook.coml>
2025-03-23 15:49:51 +0900

    Jungol #9419

---
+++ Jungol#9419.py
@@ -0,0 +1,15 @@
+l1 = [1, 1, 1, 1, 1]
+l2 = [1, 2, 3, 4, 5]
+l3 = [1, 3, 6, 10, 15]
+l4 = [1, 4, 10, 20, 35]
+l5 = [1, 5, 15, 35, 70]
+for i in l1: print(i, end=' ')
+print('')
+for i in l2: print(i, end=' ')
+print('')
+for i in l3: print(i, end=' ')
+print('')
+for i in l4: print(i, end=' ')
+print('')
+for i in l5: print(i, end=' ')
+print('')
