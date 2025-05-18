bd9f26d04ca9acb49695845874821d96d77b0230
sihyeonkim <bingusean@outlook.coml>
2025-05-18 14:34:58 +0900

    Jungol #9425

---
+++ Jungol#9425.py
@@ -0,0 +1,17 @@
+i1 = input().split()
+i2 = input().split()
+i3 = input().split()
+
+utl = {'A':'a','B':'b','C':'c','D':'d','E':'e','F':'f','G':'g','H':'h','I':'i','J':'j','K':'k','L':'l','M':'m','N':'n','O':'o','P':'p','Q':'q','R':'r','S':'s','T':'t','U':'u','V':'v','W':'w','X':'x','Y':'y','Z':'z'}
+
+for s in range(5):
+    print(utl[i1[s]], end=" ")
+print("")
+
+for s in range(5):
+    print(utl[i2[s]], end=" ")
+print("")
+
+for s in range(5):
+    print(utl[i3[s]], end=" ")
+print("")
