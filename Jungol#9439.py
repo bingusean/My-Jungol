bda5aa8981f57224aad4a1b050ecb66eeab3dc2b
sihyeonkim <bingusean@outlook.coml>
2025-06-08 14:12:04 +0900

    Jungol#9439

---
+++ Jungol#9439.py
@@ -0,0 +1,7 @@
+I1 = int(input())
+I2 = int(input())
+I3 = int(input())
+Alex = {'korean' : 95, 'english' : 90, 'math' : 80}
+Bill = {'korean' : I1, 'english' : I2, 'math' : I3}
+print("Alex:",Alex)
+print("Bill:",Bill)
