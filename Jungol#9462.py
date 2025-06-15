0dc7d97d0fd39cc37661893adf07c1e566179ee1
sihyeonkim <bingusean@outlook.coml>
2025-06-15 15:38:17 +0900

    Jungol#9462

---
+++ Jungol#9462.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        print(arr)
+I = int(input())
+J = list(map(int, input().split()))
+bubble_sort(J)
