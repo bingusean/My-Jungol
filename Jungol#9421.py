10c001839401f848b4f4c9155bc5142e0a94cf5f
sihyeonkim <bingusean@outlook.coml>
2025-07-13 15:02:14 +0900

    Make Jungol#9421 Better

--- Jungol#9421.py
+++ Jungol#9421.py
@@ -1,62 +1,6 @@
-i1 = input()
-i2 = input()
-i3 = input()
-i4 = input()
-i5 = input()
-i6 = input()
-
-c1 = 0
-c2 = 0
-c3 = 0
-c4 = 0
-c5 = 0
-c6 = 0
-
-if i1 == "1": c1 += 1
-if i1 == "2": c2 += 1
-if i1 == "3": c3 += 1
-if i1 == "4": c4 += 1
-if i1 == "5": c5 += 1
-if i1 == "6": c6 += 1
-
-if i2 == "1": c1 += 1
-if i2 == "2": c2 += 1
-if i2 == "3": c3 += 1
-if i2 == "4": c4 += 1
-if i2 == "5": c5 += 1
-if i2 == "6": c6 += 1
-
-if i3 == "1": c1 += 1
-if i3 == "2": c2 += 1
-if i3 == "3": c3 += 1
-if i3 == "4": c4 += 1
-if i3 == "5": c5 += 1
-if i3 == "6": c6 += 1
-
-if i4 == "1": c1 += 1
-if i4 == "2": c2 += 1
-if i4 == "3": c3 += 1
-if i4 == "4": c4 += 1
-if i4 == "5": c5 += 1
-if i4 == "6": c6 += 1
-
-if i5 == "1": c1 += 1
-if i5 == "2": c2 += 1
-if i5 == "3": c3 += 1
-if i5 == "4": c4 += 1
-if i5 == "5": c5 += 1
-if i5 == "6": c6 += 1
-
-if i6 == "1": c1 += 1
-if i6 == "2": c2 += 1
-if i6 == "3": c3 += 1
-if i6 == "4": c4 += 1
-if i6 == "5": c5 += 1
-if i6 == "6": c6 += 1
-
-print("1: " + str(c1))
-print("2: " + str(c2))
-print("3: " + str(c3))
-print("4: " + str(c4))
-print("5: " + str(c5))
-print("6: " + str(c6))
+count = [0] * 7
+for _ in range(6):
+    num = int(input())
+    count[num] += 1
+for i in range(1, 7):
+    print(f"{i}: {count[i]}")
